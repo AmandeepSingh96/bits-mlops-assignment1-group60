d93aff6829534045067abf76c7ddf01448936786
AmandeepSingh96 <deepaman9886@gmail.com>
2025-01-11 14:58:07 +0530

    Linting Fix - Added two Blank lines as expected

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 
+
 def train_model():
     X, y = load_diabetes(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
--- test_model.py
+++ test_model.py
@@ -1,6 +1,7 @@
 # test_model.py
 from model import train_model
 
+
 def test_train_model():
     score = train_model()
     assert score > 0.7, "Model accuracy should be greater than 0.7"
