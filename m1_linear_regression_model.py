b0dc86fac31152e4b8c3297f5537515640cca292
AmandeepSingh96 <deepaman9886@gmail.com>
2025-01-11 22:40:21 +0530

    Linting Fix - Updated Code to not exceed line length

--- m1_linear_regression_model.py
+++ m1_linear_regression_model.py
@@ -1,6 +1,4 @@
-import os
 import pandas as pd
-from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
@@ -14,7 +12,9 @@ def train_model():
     y = df['target'].values
 
     # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
     # Create and train the model
     model = LinearRegression()
--- m2_mlflow_experiment.py
+++ m2_mlflow_experiment.py
@@ -12,13 +12,18 @@ df = pd.read_csv('data/diabetes.csv')
 X = df.drop(columns=['target']).values
 y = df['target'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # List of different hyperparameter settings for different runs
 params_list = [
-    {"n_estimators": 50, "max_depth": 5, "min_samples_split": 2, "min_samples_leaf": 1},
-    {"n_estimators": 100, "max_depth": 10, "min_samples_split": 5, "min_samples_leaf": 2},
-    {"n_estimators": 150, "max_depth": 15, "min_samples_split": 10, "min_samples_leaf": 4}
+    {"n_estimators": 50, "max_depth": 5,
+     "min_samples_split": 2, "min_samples_leaf": 1},
+    {"n_estimators": 100, "max_depth": 10,
+     "min_samples_split": 5, "min_samples_leaf": 2},
+    {"n_estimators": 150, "max_depth": 15,
+     "min_samples_split": 10, "min_samples_leaf": 4}
 ]
 
 # Loop over different parameter settings
@@ -53,8 +58,11 @@ for params in params_list:
         input_example = X_train[:5]
 
         # Log the model with the input example
-        mlflow.sklearn.log_model(model, "random_forest_model", input_example=input_example)
+        mlflow.sklearn.log_model(
+            model, "random_forest_model", input_example=input_example
+        )
 
-        print(f"Run with parameters {params} has MSE: {mse:.4f}, MAE: {mae:.4f}, R²: {r2:.4f}")
+        print(f"Run with parameters {params} has MSE:"
+              f" {mse:.4f}, MAE: {mae:.4f}, R²: {r2:.4f}")
 
 # End the runs
